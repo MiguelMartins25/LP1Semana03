4e09b7d43d942fdb647dab1c2665c915c49278eb
Miguel Martins <miguelarbmartins@gmail.com>
2026-03-05 13:30:24 +0000

    Exercicio 1

---
+++ RightSize/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach(string i in args)
+            {
+                // Counts the number of letters in each string
+                int stringlength = i.Length;
+
+                // Sees if the number of letters in the string is above 3
+                if (stringlength <= 3)
+                {
+                    continue;
+                }
+
+                // Print the string
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
