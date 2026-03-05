10c3a0f53743deee3069e55379b2f89bb4104aa6
Miguel Martins <miguelarbmartins@gmail.com>
2026-03-05 14:10:49 +0000

    Exercicio 2

---
+++ ChangeString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Please input a phrase:");
+            string fullstring = Console.ReadLine();
+            Console.Write("Input a letter:");
+            string letter = Console.ReadLine();
+            char charletter = char.Parse(letter);
+            foreach (char i in fullstring)
+            {
+                if (i == charletter)
+                {
+                    Console.Write("x");
+                }
+                else
+                {
+                    Console.Write(i);
+                }
+            }
+        }
+    }
+}
