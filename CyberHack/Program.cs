768c9c319e524030c89ab55da343981073263f68
Miguel Martins <miguelarbmartins@gmail.com>
2026-03-05 16:22:19 +0000

    Added User Inputs

--- CyberHack/Program.cs
+++ CyberHack/Program.cs
@@ -5,26 +5,39 @@ namespace CyberHack
     [Flags]
     enum HackTool
     {
-        BruteForce = 0,
-        Phishing = 1,
-        Backdoor = 2,
-        ZeroDay = 4,
-        AIOverride = 8,
-        QuantumExploit = 9
+        None = 0,
+        BruteForce = 1,
+        Phishing = 2,
+        Backdoor = 4,
+        ZeroDay = 8,
+        AIOverride = 16,
+        QuantumExploit = 32
     }
 
     enum SystemType
     {
-        CorporateServer = 0,
-        BankDatabase = 1,
-        SmartCityCore = 2,
-        MilitaryAI = 4
+        CorporateServer,
+        BankDatabase,
+        SmartCityCore,
+        MilitaryAI
     }
     public class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            // Codigo para ler qual o SystemType a hackear
+            SystemType system = Enum.Parse<SystemType>(Console.ReadLine());
+            // Codigo que divide cada HackingTool e adiciona-as a um array
+            string[] HackingTool = Console.ReadLine().Split(" ");
+
+            HackTool tools = HackTool.None;
+            
+            // Foreach para transformar cada HackingTool em uma variável
+            foreach (string t in HackingTool)
+            {
+                tools |= Enum.Parse<HackTool>(t);
+            }
+
         }
     }
 }
